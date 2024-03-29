03cd2c625f2fb7ab1a41800cde382e1dc5889019
Salisu Aminu Abdurrahman <salisuaminu615@gmail.com>
2024-03-29 14:45:57 +0100

    updated search

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
 const logger = require('../logger');
-
 // Get all gifts
 router.get('/', async (req, res, next) => {
     logger.info('/ called');
     try {
         const db = await connectToDatabase();
-
         const collection = db.collection("gifts");
         const gifts = await collection.find({}).toArray();
         res.json(gifts);
@@ -17,7 +15,6 @@ router.get('/', async (req, res, next) => {
         next(e);
     }
 });
-
 // Get a single gift by ID
 router.get('/:id', async (req, res, next) => {
     try {
@@ -25,29 +22,23 @@ router.get('/:id', async (req, res, next) => {
         const collection = db.collection("gifts");
         const id = req.params.id;
         const gift = await collection.findOne({ id: id });
-
         if (!gift) {
             return res.status(404).send("Gift not found");
         }
-
         res.json(gift);
     } catch (e) {
         next(e);
     }
 });
-
-
 // Add a new gift
 router.post('/', async (req, res, next) => {
     try {
         const db = await connectToDatabase();
         const collection = db.collection("gifts");
         const gift = await collection.insertOne(req.body);
-
         res.status(201).json(gift.ops[0]);
     } catch (e) {
         next(e);
     }
 });
-
 module.exports = router;
