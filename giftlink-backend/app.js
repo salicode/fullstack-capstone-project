ee551148fcd0fd9c444bfaa1e4f880622e428a86
salicode <salisuaminu615@gmail.com>
2024-03-31 08:59:53 -0400

    auth

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,33 +1,16 @@
-// app.js
+/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
-const { MongoClient } = require('mongodb');
-const path = require('path');
-
-const pinoLogger = require('./logger')
+const cors = require('cors');
+const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
+const {loadData} = require("./util/import-mongo/index");
+
 
 const app = express();
-const port = 3050;
-
-// Serve static files from the public directory (for home.html)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve static files for React App from a subdirectory
-app.use('/app', express.static(path.join(__dirname, 'public', 'react-app')));
-
-
-// Route for Home Page - Serve home.html as the default page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
-});
-
-// Serve the React app's index.html for any other requests under /app
-app.get('/app/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'react-app', 'index.html'));
-});
-
+app.use("*",cors());
+const port = 3060;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
@@ -40,7 +23,7 @@ app.use(express.json());
 
 // Route files
 const giftRoutes = require('./routes/giftRoutes');
-// const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -49,19 +32,18 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 app.use('/api/gifts', giftRoutes);
-// app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
-
-// Default Route for React App
-app.get('/app/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'react-app', 'index.html'));
-});
 
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
+
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
