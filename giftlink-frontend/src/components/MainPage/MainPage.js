c5db54ce600bdfb70e823368e33db78d4e52dffd
Salisu Aminu Abdurrahman <30727466+salicode@users.noreply.github.com>
2024-04-27 23:57:34 +0100

    Update MainPage.js

--- giftlink-frontend/src/components/MainPage/MainPage.js
+++ giftlink-frontend/src/components/MainPage/MainPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import {urlConfig} from '../../config';
-import { urlConfig } from './config';
+import {urlConfig} from '../../config';
 
 
 function MainPage() {
