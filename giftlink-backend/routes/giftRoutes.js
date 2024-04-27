fb75365365a7989b1f5a7c3333d4d3eeae09c2fb
Salisu Aminu Abdurrahman <30727466+salicode@users.noreply.github.com>
2024-04-27 21:35:40 +0100

    Update giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -2,8 +2,55 @@ const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
 const logger = require('../logger');
+// // Get all gifts
+// router.get('/', async (req, res, next) => {
+//     logger.info('/ called');
+//     try {
+//         const db = await connectToDatabase();
+//         const collection = db.collection("gifts");
+//         const gifts = await collection.find({}).toArray();
+//         res.json(gifts);
+//     } catch (e) {
+//         logger.console.error('oops something went wrong', e)
+//         next(e);
+//     }
+// });
+// // Get a single gift by ID
+// router.get('/:id', async (req, res, next) => {
+//     try {
+//         const db = await connectToDatabase();
+//         const collection = db.collection("gifts");
+//         const id = req.params.id;
+//         const gift = await collection.findOne({ id: id });
+//         if (!gift) {
+//             return res.status(404).send("Gift not found");
+//         }
+//         res.json(gift);
+//     } catch (e) {
+//         next(e);
+//     }
+// });
+// // Add a new gift
+// router.post('/', async (req, res, next) => {
+//     try {
+//         const db = await connectToDatabase();
+//         const collection = db.collection("gifts");
+//         const gift = await collection.insertOne(req.body);
+//         res.status(201).json(gift.ops[0]);
+//     } catch (e) {
+//         next(e);
+//     }
+// });
+// module.exports = router;
+/* jshint esversion: 6 */
+
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const logger = require('../logger');
+
 // Get all gifts
-router.get('/', async (req, res, next) => {
+router.get('/', async function(req, res, next) {
     logger.info('/ called');
     try {
         const db = await connectToDatabase();
@@ -11,12 +58,13 @@ router.get('/', async (req, res, next) => {
         const gifts = await collection.find({}).toArray();
         res.json(gifts);
     } catch (e) {
-        logger.console.error('oops something went wrong', e)
+        logger.console.error('oops something went wrong', e);
         next(e);
     }
 });
+
 // Get a single gift by ID
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', async function(req, res, next) {
     try {
         const db = await connectToDatabase();
         const collection = db.collection("gifts");
@@ -30,8 +78,9 @@ router.get('/:id', async (req, res, next) => {
         next(e);
     }
 });
+
 // Add a new gift
-router.post('/', async (req, res, next) => {
+router.post('/', async function(req, res, next) {
     try {
         const db = await connectToDatabase();
         const collection = db.collection("gifts");
@@ -41,4 +90,5 @@ router.post('/', async (req, res, next) => {
         next(e);
     }
 });
+
 module.exports = router;
